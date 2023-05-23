3e3d0b5e0a6c64dbbcce0c6625108b9f40b8ddbd
Amersfoort <amersfoortlam@gmail.com>
2023-05-23 23:25:05 +0800

    Update my Header, working on content

--- part 1/src/App.js
+++ part 1/src/App.js
@@ -1,27 +1,50 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {name :'Half Stack application development',
+
+  part : [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
+}
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
+      <Header course = {course.name}/>
+      <Content content = {course.part}/>
+    </div>
+  )
+  
+}
+
+
+const Header = ({course}) => {
+return (
+  <h1>{course}</h1>
+)
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p> 
+        <Part part ={props.part[0].name} exercises = {props.part[0].exercises}/>
+        <Part part ={props.part[1].name} exercises = {props.part[1].exercises}/>
+        <Part part ={props.part[2].name} exercises = {props.part[2].exercises}/>
       </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.part[0].exercises+props.part[1].exercises+props.part[2].exercises}</p>
+  )
+}
 export default App
